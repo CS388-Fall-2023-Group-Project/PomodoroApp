a0ba52df232a7378ec6f029b137ed101ebb69883
Nguyen Thai Thao Uyen (Chloe) <thaouyen.nttu@gmail.com>
2023-11-16 17:02:18 -0500

    Reset main branch

--- app/src/main/java/com/example/pomodoro/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro/MainActivity.kt
@@ -19,10 +19,5 @@ class MainActivity : AppCompatActivity() {
         btnAnalyticsTab = findViewById(R.id.TabAnalyticsButton)
         btnHistoryTab = findViewById(R.id.TabHistoryButton)
 
-        // INTENT to History
-        btnHistoryTab.setOnClickListener {
-            val intent = Intent(this, History::class.java)
-            startActivity(intent)
-        }
     }
 }
