94b355a39d9e51ad95ac078eebcf690a8ab7929a
Nguyen Thai Thao Uyen (Chloe) <thaouyen.nttu@gmail.com>
2023-11-17 18:25:43 -0500

    Major change to Bottom Nav Bar
    I change this on main branch so we can use it together. This shouldn't affect your work on your fragment.

--- app/src/main/java/com/example/pomodoro/MainActivity.kt
+++ app/src/main/java/com/example/pomodoro/MainActivity.kt
@@ -4,20 +4,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnStartStudyingTab: Button
-    private lateinit var btnAnalyticsTab: Button
-    private lateinit var btnHistoryTab: Button
-
+    private val onNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    loadFragment(Fragment_Home())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.navigation_history -> {
+                    loadFragment(Fragment_Home())
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Load other fragment using nav bar
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavBar)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
-        btnStartStudyingTab = findViewById(R.id.TabStartStudyingButton)
-        btnAnalyticsTab = findViewById(R.id.TabAnalyticsButton)
-        btnHistoryTab = findViewById(R.id.TabHistoryButton)
-
+        // Load the initial fragment
+        loadFragment(Fragment_Home())
+    }
+    // Method to load a fragment
+    fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
