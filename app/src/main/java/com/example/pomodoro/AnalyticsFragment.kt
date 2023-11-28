bff85e39453f535f1c4a801ef59fca336fd74d81
Nrodriguez23 <144366574+Nrodriguez23@users.noreply.github.com>
2023-11-28 16:54:36 -0500

    Added and Fixed Charts

--- app/src/main/java/com/example/pomodoro/AnalyticsFragment.kt
+++ app/src/main/java/com/example/pomodoro/AnalyticsFragment.kt
@@ -1,4 +1,5 @@
 package com.example.pomodoro
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,40 +11,68 @@ import com.example.pomodoro.utils.VerticalChartXLabels
 import com.example.pomodoro.utils.horizontalChartData
 import com.example.pomodoro.utils.verticalChartData
 import com.straiberry.android.charts.tooltip.PointTooltip
+import com.straiberry.android.charts.view.BarChartView
+import com.straiberry.android.charts.view.BarPercentChartView
 import com.straiberry.android.charts.view.HorizontalBarChartView
 import java.util.Calendar
 import java.util.Date
 
 class AnalyticsFragment : Fragment() {
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_analytics, container, false)
     }
-// this is a comment to teach about git
+
+    // this is a comment to teach about git
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setupHorizontalChart(view)
         setupBarChart(view)
-        setupBarPercentChart()
+        setupBarPercentChart(view)
     }
-// Comment
+
+    // Comment
     private fun setupHorizontalChart(view: View) {
         val data = horizontalChartData(
             requireContext(), HorizontalChartXLabels(
-                ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_background)!!, "History",
-                ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_background)!!, "English",
-                ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_background)!!, "Math",
-                ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_background)!!, "Science"
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_background)!!,
+                "History",
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_background)!!,
+                "English",
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_background)!!,
+                "Math",
+                ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_background)!!,
+                "Science"
             ), listOf(6F, 7F, 2F, 6F)
         )
         val horizontalBarChartViewBrushingDigit =
             view.findViewById<HorizontalBarChartView>(R.id.horizontalBarChartViewBrushingDigit)
         horizontalBarChartViewBrushingDigit.animate(data)
+    }
+
+    private fun setupBarPercentChart(view: View) {
+        // Prepare the tooltip to show on chart
+        val pointTooltip = PointTooltip()
+        pointTooltip.onCreateTooltip(view.findViewById(R.id.parentView))
+
+        val barPercentChartWhitening =
+            view.findViewById<BarPercentChartView>(R.id.barPercentChartWhitening)
+
+
+        barPercentChartWhitening.apply {
+            tooltip = pointTooltip
+            currentAverage = 20
+            average = 50
+            previousAverage = 30
+            createBarPercent()
+            disableTouchAndClick()
+        }
     }
 
     private fun setupBarPercentChart() {
@@ -62,6 +91,8 @@ class AnalyticsFragment : Fragment() {
                 "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
             ), listOf(6F, 7F, 2F, 6F, 7F, 7F, 7F)
         )
-        // Add your bar chart setup code here
+        val barChartViewBrushing =
+            view.findViewById<BarChartView>(R.id.barChartViewBrushing)
+        barChartViewBrushing.animate(data)
     }
 }
