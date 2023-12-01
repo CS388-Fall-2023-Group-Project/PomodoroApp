17c74660e3a6f5b4d4304b0f8fd33926d654a07a
mgall <mgallo185@gmail.com>
2023-12-01 14:22:41 -0500

    adding eddies code to breakactivity

--- app/src/main/java/com/example/pomodoro/BreakActivity.kt
+++ app/src/main/java/com/example/pomodoro/BreakActivity.kt
@@ -6,20 +6,24 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class BreakActivity: AppCompatActivity() {
     private lateinit var progressBar: ProgressBar
     private lateinit var timerText: TextView
     private lateinit var countdownTimer: CountdownTimerHelper
     private lateinit var exitButton: Button
-    override fun onCreate(savedInstanceState: Bundle?) {
+   /* override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.challenege_list)
 
 
         progressBar = findViewById(R.id.studyoffTimer)
         timerText = findViewById(R.id.timerTextView)
-        exitButton = findViewById(R.id.return_btn)
+        exitButton = findViewById(R.id)
 
 
 
@@ -30,5 +34,46 @@ class BreakActivity: AppCompatActivity() {
             startActivity(intent)
 
         }
+    } */
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.yoga_list)
+
+
+        val webView: WebView = findViewById(R.id.webView)
+        exitButton = findViewById(R.id.return_btn)
+
+        // Enable JavaScript in the WebView
+        webView.settings.javaScriptEnabled = true
+
+        // Load the URL with the iframe content
+        val iframeContent = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/SvPKFsCiMsw?si=aX39cuiOMQhc6voH\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>"
+        webView.loadData(iframeContent, "text/html", "utf-8")
+
+        // Set a WebViewClient to handle navigation events within the WebView
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                view?.loadUrl(request?.url.toString())
+                return true
+            }
+        }
+
+        // Set a WebChromeClient to handle UI-related events
+        webView.webChromeClient = object : WebChromeClient() {
+            // Handle UI events here if needed
+        }
+
+        exitButton.setOnClickListener {
+            // Navigate back to the home fragment or activity
+            val returnStudy= Intent(this@BreakActivity,TimerActivity::class.java)
+            returnStudy.putExtra("restartTimer", true)
+            startActivity(returnStudy)
+
+        }
     }
+
+
+
 }
