05a61e7c7bc7b240cbacc868993f11811add69fc
mgall <mgallo185@gmail.com>
2023-12-12 16:13:12 -0500

    confetti animation works

--- app/src/main/java/com/example/pomodoro/CongratsActivity.kt
+++ app/src/main/java/com/example/pomodoro/CongratsActivity.kt
@@ -2,21 +2,23 @@ package com.example.pomodoro
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.FrameLayout
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.github.jinatonic.confetti.CommonConfetti
 import android.graphics.Color
-import android.widget.Button
-import android.widget.RelativeLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class CongratsActivity : AppCompatActivity(){
+class CongratsActivity : AppCompatActivity() {
+    private lateinit var confettiContainer: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.congrats_screen)
-        val confettiContainer= findViewById<ConstraintLayout>(R.id.confetti)
-         CommonConfetti.rainingConfetti(confettiContainer, intArrayOf(Color.RED, Color.GREEN, Color.BLUE)).oneShot()
+
+        confettiContainer = findViewById(R.id.confetti)
 
         // Update the congratulatory message
         val congratsMessage = findViewById<TextView>(R.id.congratsMessage)
@@ -24,13 +26,34 @@ class CongratsActivity : AppCompatActivity(){
 
         val returnHome = findViewById<Button>(R.id.gotoHome)
 
-
         returnHome.setOnClickListener {
             val gotoHomescreen = Intent(this@CongratsActivity, MainActivity::class.java)
             startActivity(gotoHomescreen)
         }
 
+        // Start the confetti animation
+        startConfettiAnimation()
+    }
 
+    private fun startConfettiAnimation() {
+        val handler = Handler()
+        val delay: Long = 1000 // Set the delay between each confetti animation (in milliseconds)
+
+        val runnable = object : Runnable {
+            override fun run() {
+                // Trigger confetti animation
+                CommonConfetti.rainingConfetti(
+                    confettiContainer,
+                    intArrayOf(Color.RED, Color.GREEN, Color.BLUE)
+                ).oneShot()
+
+                // Repeat the animation
+                handler.postDelayed(this, delay)
+            }
+        }
+
+        // Start the initial confetti animation
+        handler.postDelayed(runnable, delay)
     }
 
     // Add other logic as needed
