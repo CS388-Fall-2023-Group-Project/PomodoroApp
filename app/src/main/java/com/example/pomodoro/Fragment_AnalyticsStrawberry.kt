e6c74218eda9d857fec644c22eccf002c9dc5323
Nguyen Thai Thao Uyen (Chloe) <96941717+pengwingokla@users.noreply.github.com>
2023-12-14 01:14:44 -0500

    Fragment_AnalyticsStrawberry can now update streak

--- app/src/main/java/com/example/pomodoro/Fragment_AnalyticsStrawberry.kt
+++ app/src/main/java/com/example/pomodoro/Fragment_AnalyticsStrawberry.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class Fragment_AnalyticsStrawberry : Fragment() {
+
+    private lateinit var mainDatabase: MainDatabase
+    private var currentStreak: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,21 @@ class Fragment_AnalyticsStrawberry : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_analytics_strawberry, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainDatabase = MainDatabase(requireContext())
+
+        currentStreak = mainDatabase.calculateStudyStreak()
+        updateStreak()
+
+    }
+
+    private fun updateStreak() {
+        val textViewStreak: TextView = requireView().findViewById(R.id.t2)
+        textViewStreak.text = "🔥🔥 Your on a $currentStreak Study Streak!!! 🔥🔥"
     }
 
 }
